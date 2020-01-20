9f0955c181149fa733414a72be7e405bc7fa6884
raojing <1770561005@qq.com>
2020-01-20 11:21:40 +0800

    各服务表结构

---
+++ rain-log/src/main/resources/rain_log_dev.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.33 : Database - rain_log_dev
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rain_log_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `rain_log_dev`;
+
+/*Table structure for table `user_login_logout_log` */
+
+DROP TABLE IF EXISTS `user_login_logout_log`;
+
+CREATE TABLE `user_login_logout_log` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `user_name` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `phone` varchar(100) DEFAULT NULL COMMENT '手机号码',
+  `type` tinyint(1) DEFAULT NULL COMMENT '类型 1-登录 2-登出',
+  `ip` varchar(20) DEFAULT NULL COMMENT 'ip',
+  `country` varchar(100) DEFAULT NULL COMMENT '国家',
+  `province` varchar(100) DEFAULT NULL COMMENT '省份',
+  `city` varchar(100) DEFAULT NULL COMMENT '城市',
+  `area` varchar(100) DEFAULT NULL COMMENT '区县',
+  `isp` varchar(100) DEFAULT NULL COMMENT '互联网服务提供商',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `user_type` varchar(20) DEFAULT NULL COMMENT '用户类型',
+  `wx_openid` varchar(100) DEFAULT NULL COMMENT '微信openID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统用户登录登出日志';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ rain-user/src/main/resources/rain_user_dev.sql
@@ -0,0 +1,223 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.33 : Database - rain_user_dev
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rain_user_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `rain_user_dev`;
+
+/*Table structure for table `clientdetails` */
+
+DROP TABLE IF EXISTS `clientdetails`;
+
+CREATE TABLE `clientdetails` (
+  `appId` varchar(128) NOT NULL,
+  `resourceIds` varchar(256) DEFAULT NULL,
+  `appSecret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `grantTypes` varchar(256) DEFAULT NULL,
+  `redirectUrl` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additionalInformation` varchar(4096) DEFAULT NULL,
+  `autoApproveScopes` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`appId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_access_token` */
+
+DROP TABLE IF EXISTS `oauth_access_token`;
+
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_approvals` */
+
+DROP TABLE IF EXISTS `oauth_approvals`;
+
+CREATE TABLE `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NULL DEFAULT NULL,
+  `lastModifiedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_client_details` */
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(128) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_client_token` */
+
+DROP TABLE IF EXISTS `oauth_client_token`;
+
+CREATE TABLE `oauth_client_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_code` */
+
+DROP TABLE IF EXISTS `oauth_code`;
+
+CREATE TABLE `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_refresh_token` */
+
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `rain_member` */
+
+DROP TABLE IF EXISTS `rain_member`;
+
+CREATE TABLE `rain_member` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `user_name` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `phone` varchar(50) DEFAULT NULL COMMENT '电话号码',
+  `password` varchar(300) DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(100) DEFAULT NULL COMMENT '昵称',
+  `wx_openid` varchar(100) DEFAULT NULL COMMENT '微信openID',
+  `wx_nickname` varchar(100) DEFAULT NULL COMMENT '微信昵称',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `avatar` varchar(200) DEFAULT NULL COMMENT '头像',
+  `gender` tinyint(1) DEFAULT NULL COMMENT '性别 1-男 2-女',
+  `status` tinyint(1) DEFAULT NULL COMMENT '状态',
+  `personal_signature` varchar(500) DEFAULT NULL COMMENT '个性签名',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会员表';
+
+/*Table structure for table `rain_member_wallet` */
+
+DROP TABLE IF EXISTS `rain_member_wallet`;
+
+CREATE TABLE `rain_member_wallet` (
+  `member_id` bigint(20) NOT NULL COMMENT '用户id',
+  `pay_password` varchar(100) DEFAULT NULL COMMENT '支付密码',
+  `balance` bigint(20) DEFAULT NULL COMMENT '可用余额(分)',
+  `history_balance` bigint(20) DEFAULT NULL COMMENT '历史余额(分)',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会员钱包表';
+
+/*Table structure for table `rain_permission` */
+
+DROP TABLE IF EXISTS `rain_permission`;
+
+CREATE TABLE `rain_permission` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `permission_name` varchar(200) DEFAULT NULL COMMENT '权限名称',
+  `permission_code` varchar(200) DEFAULT NULL COMMENT '权限码',
+  `permission_desc` varchar(200) DEFAULT NULL COMMENT '权限描述',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父id',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
+
+/*Table structure for table `rain_role` */
+
+DROP TABLE IF EXISTS `rain_role`;
+
+CREATE TABLE `rain_role` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `role_name` varchar(200) DEFAULT NULL COMMENT '角色名称',
+  `role_code` varchar(200) DEFAULT NULL COMMENT '角色标识',
+  `role_desc` varchar(200) DEFAULT NULL COMMENT '角色描述',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+/*Table structure for table `rain_role_permission` */
+
+DROP TABLE IF EXISTS `rain_role_permission`;
+
+CREATE TABLE `rain_role_permission` (
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  `permission_id` bigint(20) NOT NULL COMMENT '权限id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限关系表';
+
+/*Table structure for table `rain_system_user` */
+
+DROP TABLE IF EXISTS `rain_system_user`;
+
+CREATE TABLE `rain_system_user` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `user_name` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `phone` varchar(50) DEFAULT NULL COMMENT '手机号码',
+  `password` varchar(300) DEFAULT NULL COMMENT '登录密码',
+  `nick_name` varchar(100) DEFAULT NULL COMMENT '用户昵称',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `avatar` varchar(200) DEFAULT NULL COMMENT '头像',
+  `status` tinyint(1) DEFAULT NULL COMMENT '状态 1-启用 2-禁用 3-锁定 4-删除',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `delete_time` datetime DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统用户表';
+
+/*Table structure for table `rain_user_role` */
+
+DROP TABLE IF EXISTS `rain_user_role`;
+
+CREATE TABLE `rain_user_role` (
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户角色关系表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
